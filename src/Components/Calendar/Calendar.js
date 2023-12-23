11e7216c58063094539a9351683d611bff1c331f
Pravallika-Akula <pravallika.akula@gmail.com>
2023-12-23 15:24:58 -0600

    Added some stuff on the calendar and past entries pages

--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./Calendar.css";
 
 const Calendar = () => {
-    return <h1>Calendar</h1>;
+    return <h1 className="Calendar">Calendar</h1>;
 };
   
 export default Calendar;
