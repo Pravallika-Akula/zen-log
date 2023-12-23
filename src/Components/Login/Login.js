4002c57d13049c70472f9ec8a4de1b8bc45cd5f1
Varshni <varshnik@gmail.com>
2023-12-23 15:20:14 -0600

    created ui

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,7 +1,47 @@
 import React from "react";
+import "./Login.css";
+import {Link} from "react-router-dom";
 
 const Login = () => {
-    return <h1>Login</h1>;
+    return (
+        <div>
+            <head>
+                <link href='https://fonts.googleapis.com/css?family=Nunito Sans' rel='stylesheet' />
+            </head>
+            <body>
+                <div className="logo"></div>
+                <div className="login">
+                    <h1 className="login-header">Welcome Back!</h1> 
+                    <p className="login-subheader">Please log in to begin writing</p> 
+                </div>
+                <div className="form-items">
+                    <div className="email-address">
+                        <label className="field-header">Email</label>
+                        <input type="text" id="email" placeholder="you@email.com" />
+                    </div>
+                    <div className="password">
+                        <Link to="/dashboard">
+                            <p className="forgot-password">forgot password?</p>
+                        </Link>    
+                        <label className="field-header">Password</label>
+                        <input type="text" id="password" placeholder="Enter 6 characters or more" />
+                    </div>
+                    <div className="remember-me">
+                        <label className="field-header-remember">Remember Me
+                            <input type="checkbox" id="remember" />
+                        </label>
+                    </div>
+                    <Link to="/dashboard">
+                        <button className="login-button">Login</button>
+                    </Link>
+                </div>
+                <Link to="/sign-up">
+                    <p className="sign-up">Don't have an account? <b>Sign Up</b></p>
+                </Link> 
+            </body>
+            <img src="./Vector.png" alt="leaf pic"></img>
+        </div>  
+    );
 };
   
 export default Login;
