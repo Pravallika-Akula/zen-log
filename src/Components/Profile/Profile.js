930a0c301e0128e15de743df1fb8a494d5408d05
Pravallika-Akula <pravallika.akula@gmail.com>
2024-01-12 18:53:19 -0600

    get user info from firestore to profile

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -9,54 +9,35 @@ import {collection, getDocs, addDoc, query, where} from 'firebase/firestore';
 
 let totalEntries = 31;
 let totalMoods = 28;
-let userName = 'Varshni';
-let userPass = '123secure';
 let email = "";
-let firstname = "";
-let lastname = "";
-let password = "";
 
 const Profile = () => {
     
-    // const usersCollectionRef = collection(db, "users")
-    const [users, setUsers] = useState([]);
-    const [value, setValue] = useState(password);
+    // const [users, setUsers] = useState([]);
+    const [value, setValue] = useState("");
     const auth = getAuth()
     const user = auth.currentUser;
-    // const q = query(usersCollectionRef, where("email", "==", email))
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [password, setPassword] = useState("");
 
-    // async function someFunction() {
-    //     console.log("hi")
-    //     const querySnapshot = await getDocs(q);
-    //     querySnapshot.docs.forEach((doc) => {
-    //         console.log(doc.id, " => ", doc.data());
-    //         password = doc.data().password;
-    //     });
-    //     console.log(querySnapshot.docs[0].data());
-    // }
-
+    // for fetching user data
     useEffect(() => {
         const fetchData = async () => {
             if (user) {
                 const usersCollectionRef = collection(db, "users");
                 const q = query(usersCollectionRef, where("email", "==", user.email));
                 const querySnapshot = await getDocs(q);
-                console.log(querySnapshot.docs[0].data());
-                password = querySnapshot.docs[0].data().firstname;
-                console.log(password);
+                setPassword(querySnapshot.docs[0]?.data()?.password || "");
+                setFirstname(querySnapshot.docs[0]?.data()?.firstname || "");
+                setLastname(querySnapshot.docs[0]?.data()?.lastname || "");
+                // console.log(password);
             }
         };
         fetchData();
-    }, [user]);
+    }, [user, password, firstname, lastname]);
 
-    // useEffect(() => {
-    //     const getUsers = async () => {
-    //         const data = await getDocs(usersCollectionRef);
-    //         setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
-    //     }
-    //     getUsers()
-    // }, [])
-
+    // for Auth
     async function handleSignOut(){
         try {
             await signOut(auth);
@@ -65,18 +46,8 @@ const Profile = () => {
         }
     }
 
-    // onAuthStateChanged(auth, (user) => {
-    //     if (user) {
-    //         const uid = user.uid;
-    //     } else {
-
-    //     }
-    // })
     if (user != null) {
         email = user.email;
-        firstname = user.firstname;
-        lastname = user.lastname;
-        password = user.password;
     }
     
     return(
@@ -86,7 +57,7 @@ const Profile = () => {
                 <link href='https://fonts.googleapis.com/css?family=Nunito Sans' rel='stylesheet' />
             </head>
             <body>
-                <img src="/Assets/Icons/menu_button.svg" alt="menu button" classname="menu-logo"></img>
+                <img src="/Assets/Icons/menu_button.svg" alt="menu button" className="menu-logo"></img>
                 <img src="/Assets/zenlog_logo.svg" alt="logo" className="logo-profile"></img>
                 <div className="profile">
                     <h1 className="profile-header">Hey {firstname}!</h1> 
@@ -96,16 +67,16 @@ const Profile = () => {
                 <div className="p-form-items">
                     <div className="p-email-address">
                         <label className="p-field-header">Email</label>
-                        <input type="text" id="email" placeholder={email}/>
+                        <input type="text" id="email" placeholder="email@gmail.com" value={email}/>
                     </div>
                     <div className="p-full-name">
                         <label className="p-field-header">Full Name</label>
-                        <input type="text" id="fullname" placeholder={password} />
+                        <input type="text" id="fullname" placeholder="password" value={firstname + " " + lastname} />
                     </div>
                     <div className="p-password password2">    
                         <label className="password-field-header">Password</label>
                         <div className="card flex justify-content-center">
-                            <Password  value={value} onChange={(e) => setValue(e.target.value)} feedback={false} tabIndex={1} toggleMask />
+                            <Password  value={password} onChange={(e) => setValue(e.target.value)} feedback={false} tabIndex={1} toggleMask />
                         </div>    
                     </div>
                     <button>
@@ -116,7 +87,6 @@ const Profile = () => {
                     </Link>
                 </div>
 
-
                 <button className="p-signOut" onClick={() => {handleSignOut()}}>
                     <p >Sign Out of My Account</p>
                 </button>
@@ -125,11 +95,7 @@ const Profile = () => {
             </body>
         </div>  
     
-    
-    
-    
-    
     );
 };
-  
+
 export default Profile;
